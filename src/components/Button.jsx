380a14af23eea28428a303d886af417758922c4e
Saifullahchy <saifullahchy0177@gmailcom>
2025-12-23 22:00:38 +0600

    udpate: add group active on click

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,6 @@
 const Button = () => {
   return (
-    <div className="w-[340px] flex justify-center items-center py-[10px] px-[20px] rounded bg-[#FFCE22] hover:bg-[#FFD84D] cursor-pointer">
+    <div className="w-[340px] text-sm leading-[130%] text-[#1F2128] flex justify-center items-center py-[10px] px-[20px] rounded bg-[#FFCE22] hover:bg-[#FFD84D] cursor-pointer">
       Done
     </div>
   );
--- src/components/CheckInput.jsx
+++ src/components/CheckInput.jsx
@@ -11,11 +11,11 @@ const CheckInput = ({ label, onChange, checked }) => {
 
   return (
     <label
-      className="group flex items-center justify-between w-full py-2 pr-[15px] pl-[22px] cursor-pointer"
+      className="group flex items-center justify-between w-full py-2 pr-[15px] pl-[22px] cursor-pointer transition-all"
       onMouseEnter={onMouseEnterOrLeave}
       onMouseLeave={onMouseEnterOrLeave}
     >
-      <span className="text-sm text-[#1F2128] leading-[130%] select-none">
+      <span className="text-sm text-[#1F2128] leading-[130%] select-none transition-all">
         {label}
       </span>
 
@@ -27,16 +27,17 @@ const CheckInput = ({ label, onChange, checked }) => {
           onChange={(e) => onChange?.(e.target.checked)}
         />
 
-        {/* Outer box (hover/selected halo lives here) */}
         <span
-          className={
-            'flex items-center justify-center w-[35px] h-[35px] transition-all'
-          }
+          className={[
+            'flex items-center justify-center w-[35px] h-[35px] rounded-lg transition-all',
+            ' group-active:bg-[#5B7CFF]/10 group-active:ring-offset-2 group-active:ring-offset-transparent',
+          ].join(' ')}
         >
-          {/* Inner checkbox */}
           <span
             className={[
-              'flex items-center justify-center w-[23px] h-[23px] rounded-[6px]  bg-white ',
+              'flex items-center justify-center w-[23px] h-[23px] border border-[#CDCDCD] rounded-[6px]  bg-white ',
+              'relative flex items-center justify-center transition-all',
+              'w-[23px] h-[23px] rounded-[6px] bg-white',
               !checked && !hover ? 'border border-[#CDCDCD] ' : '',
             ].join(' ')}
           >
